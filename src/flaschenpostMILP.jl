3b813c57b4081bc7522d55ceafa57241b4fa1d1d
KonstantinBubolz <konstantin.bubolz@googlemail.com>
2026-08-18 21:13:48 +0200

    setup project structure

---
+++ src/flaschenpostMILP.jl
@@ -0,0 +1,14 @@
+# flaschenpostMILP.jl.jl
+# Julia Script
+
+#=
+Description: 
+Author: konstantin
+Date: 18.08.26
+=#
+
+function main()
+    println("Hello, Julia!")
+end
+
+main()
---
+++ test/test.jl
@@ -0,0 +1,14 @@
+# test.jl.jl
+# Julia Script
+
+#=
+Description: 
+Author: konstantin
+Date: 16.08.26
+=#
+
+function main()
+    println("Hello, Julia!")
+end
+
+main()
