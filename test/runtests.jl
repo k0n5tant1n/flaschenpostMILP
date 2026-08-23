8cfe0cba7bd8cee646cfa5336576ed36a148572d
KonstantinBubolz <konstantin.bubolz@googlemail.com>
2026-08-23 17:28:34 +0200

    extracted test data into testData.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,41 +6,31 @@ using JuMP
 using HiGHS
 import MathOptInterface as MOI
 
+include("testData.jl")
 include("testGraphs/graphviz.jl")
 include("testGraphs/tikz.jl")
 
-time_windows = Tuple{Int, Int}[]
-push!(time_windows, (0, 0))     # unused dummy time window of depot to enhance readability of time window constraint
-push!(time_windows, (3, 123))
-one_location_feasible = Dict(
-    "location_service_time" => 5,
-    "locations" => 1:2,
-    "time_windows" => time_windows,
-    "distance_matrix" => [0 2; 2 0]
-    )
 
 @testset "One location feasible tests" begin
     @testset "Objective Value Tests" begin
-        model = build_model(one_location_feasible)
+        model = build_model(ONE_LOCATION_FEASIBLE)
         set_silent(model)
         set_optimizer(model, () -> HiGHS.Optimizer())
         optimize!(model)
-        export_graphviz("./testGraphs/OneLocationFeasible.dot", one_location_feasible, model[:A], model[:t])
-        export_tikz("./testGraphs/OneLocationFeasible.tex", one_location_feasible, model[:A], model[:t])
+        export_graphviz("./testGraphs/OneLocationFeasible.dot", ONE_LOCATION_FEASIBLE, model[:A], model[:t])
+        export_tikz("./testGraphs/OneLocationFeasible.tex", ONE_LOCATION_FEASIBLE, model[:A], model[:t])
         @test is_solved_and_feasible(model) == true
         @test objective_value(model) == 10
     end
 
     @testset "Solution Tests" begin
-        model = build_model(one_location_feasible)
+        model = build_model(ONE_LOCATION_FEASIBLE)
         set_silent(model)
         set_optimizer(model, () -> HiGHS.Optimizer())
         optimize!(model)
         @test is_solved_and_feasible(model) == true
         A = model[:A]
         t = model[:t]
-        #print(A)
-        #print(t)
         @test value(A[1,2]) == 1
         @test value(A[2,1]) == 1
         @test value(t[2]) == 3
@@ -49,18 +39,8 @@ one_location_feasible = Dict(
 end
 
 
-time_windows = Tuple{Int, Int}[]
-push!(time_windows, (0, 0))     # unused dummy time window of depot to enhance readability of time window constraint
-push!(time_windows, (1, 4))
-one_location_infeasible = Dict(
-"location_service_time" => 5,
-"locations" => 1:2,
-"time_windows" => time_windows,
-"distance_matrix" => [0 5; 5 0]
-)
-
 @testset "One location infeasible tests" begin
-    model = build_model(one_location_infeasible)
+    model = build_model(ONE_LOCATION_INFEASIBLE)
     set_silent(model)
     set_optimizer(model, () -> HiGHS.Optimizer())
     optimize!(model)
@@ -69,29 +49,16 @@ one_location_infeasible = Dict(
 end
 
 
-time_windows = Tuple{Int, Int}[]
-push!(time_windows, (0, 0))     # unused dummy time window of depot to enhance readability of time window constraint
-push!(time_windows, (3, 123))
-push!(time_windows, (10, 130))
-two_location_feasible = Dict(
-"location_service_time" => 5,
-"locations" => 1:3,
-"time_windows" => time_windows,
-"distance_matrix" => [0 5 7; 5 0 3; 7 3 0]
-)
-
 @testset "Two location feasible tests" begin
-    model = build_model(two_location_feasible)
+    model = build_model(TWO_LOCATION_FEASIBLE)
     set_silent(model)
     set_optimizer(model, () -> HiGHS.Optimizer())
     optimize!(model)
-    export_graphviz("./testGraphs/TwoLocationFeasible.dot", two_location_feasible, model[:A], model[:t])
-    export_tikz("./testGraphs/TwoLocationFeasible.tex", two_location_feasible, model[:A], model[:t])
+    export_graphviz("./testGraphs/TwoLocationFeasible.dot", TWO_LOCATION_FEASIBLE, model[:A], model[:t])
+    export_tikz("./testGraphs/TwoLocationFeasible.tex", TWO_LOCATION_FEASIBLE, model[:A], model[:t])
     @test is_solved_and_feasible(model) == true
     A = model[:A]
     t = model[:t]
-    #print(A)
-    #print(t)
     @test value(A[1,2]) == 1
     @test value(A[2,3]) == 1
     @test value(A[3,1]) == 1
---
+++ test/testData.jl
@@ -0,0 +1,43 @@
+# testData.jl.jl
+# Julia Script
+
+#=
+Description: 
+Author: konstantin
+Date: 23.08.26
+=#
+
+
+######################### ONE LOCATION FEASIBLE #############################
+time_windows = Tuple{Int, Int}[]
+push!(time_windows, (0, 0))     # unused dummy time window of depot to enhance readability of time window constraint
+push!(time_windows, (3, 123))
+const ONE_LOCATION_FEASIBLE = Dict(
+    "location_service_time" => 5,
+    "locations" => 1:2,
+    "time_windows" => time_windows,
+    "distance_matrix" => [0 2; 2 0]
+    )
+
+######################### ONE LOCATION INFEASIBLE #############################
+time_windows = Tuple{Int, Int}[]
+push!(time_windows, (0, 0))     # unused dummy time window of depot to enhance readability of time window constraint
+push!(time_windows, (1, 4))
+const ONE_LOCATION_INFEASIBLE = Dict(
+"location_service_time" => 5,
+"locations" => 1:2,
+"time_windows" => time_windows,
+"distance_matrix" => [0 5; 5 0]
+)
+
+######################### TWO LOCATION FEASIBLE #############################
+time_windows = Tuple{Int, Int}[]
+push!(time_windows, (0, 0))     # unused dummy time window of depot to enhance readability of time window constraint
+push!(time_windows, (3, 123))
+push!(time_windows, (10, 130))
+const TWO_LOCATION_FEASIBLE = Dict(
+"location_service_time" => 5,
+"locations" => 1:3,
+"time_windows" => time_windows,
+"distance_matrix" => [0 5 7; 5 0 3; 7 3 0]
+)
